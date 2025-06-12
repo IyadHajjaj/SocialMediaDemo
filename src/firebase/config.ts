ef8a6b4ced7454d6527b309d5cc2dc64486d72ee
IyadHajjaj <iyad.alhajjaj@bahcesehir.edu.tr>
2025-06-12 15:06:31 +0300

    Removed API key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,13 +8,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // Firebase configuration for your project
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+  authDomain: "socialmp-ceb72.firebaseapp.com",
+  projectId: "socialmp-ceb72",
+  storageBucket: "socialmp-ceb72.firebasestorage.app",
+  messagingSenderId: "193171957044",
+  appId: "1:193171957044:web:7183a2f81a3ecd9ea23161",
+  measurementId: "G-9E359BMTFV",
+  databaseURL: "https://socialmp-ceb72-default-rtdb.firebaseio.com"
 };
 
 // Initialize or get existing Firebase app
